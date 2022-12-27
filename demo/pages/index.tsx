02939c9bf8aa3e47bdb91e83d54a7033ca871688
erik_ritter <erik.ritter@airbnb.com>
2022-12-27 11:56:25 -0800

    Update demo site

--- demo/pages/index.tsx
+++ demo/pages/index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      Hello World. <Link href="/about">About</Link>
+      Export Data<br /><Link href="/about">About</Link>
     </div>
   )
 }
