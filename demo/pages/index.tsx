90a7e5f872a3beccdbc9101ae14fbb6ae9cfd8ac
erik_ritter <erik.ritter@airbnb.com>
2022-12-27 19:52:07 -0800

    Improve demo testing

--- demo/pages/index.tsx
+++ demo/pages/index.tsx
@@ -16,51 +16,83 @@ export default function Home() {
   }>({ columnNames: [], rows: [] });
 
   return (
-    <div>
+    <>
       <h1>Export Data</h1>
-      <label htmlFor="columnNames">Column Names</label>
-      <textarea
-        value={columnNames}
-        onChange={(e) => setColumnNames(e.target.value)}
-      />
-      <br />
-      <label htmlFor="rows">Rows</label>
-      <textarea value={rows} onChange={(e) => setRows(e.target.value)} />
-      <br />
-
-      <label htmlFor="fileType">File type</label>
-      <select
-        id="fileType"
-        value={fileType}
-        onChange={(e) => setFileType(e.target.value as FileType)}
-      >
-        <option value="CSV">CSV</option>
-        <option value="JSON">JSON</option>
-        <option value="TSV">TSV</option>
-      </select>
-      <br />
-      <label htmlFor="filename">Filename</label>
-      <input
-        id="filename"
-        type="text"
-        value={filename}
-        onChange={(e) => setFilename(e.target.value)}
-      />
-      <FileDownloadLink
-        setsDataAsyncInOnClick
-        columnNames={parsedData.columnNames}
-        rows={parsedData.rows}
-        filename={filename}
-        fileType={fileType}
-        onClick={() => {
-          setParsedData({
-            columnNames: JSON.parse(columnNames),
-            rows: JSON.parse(rows),
-          });
-        }}
-      >
-        <button>Download file</button>
-      </FileDownloadLink>
-    </div>
+      <div style={{ display: "flex", columnGap: 16 }}>
+        <div>
+          <h2>Download dynamic data</h2>
+          <label htmlFor="columnNames">Column Names</label>
+          <br />
+          <textarea
+            value={columnNames}
+            onChange={(e) => setColumnNames(e.target.value)}
+          />
+          <br />
+          <br />
+          <label htmlFor="rows">Rows</label>
+          <br />
+          <textarea
+            value={rows}
+            rows={10}
+            onChange={(e) => setRows(e.target.value)}
+          />
+          <br />
+          <br />
+          <label htmlFor="fileType">File type</label>
+          <br />
+          <select
+            id="fileType"
+            value={fileType}
+            onChange={(e) => setFileType(e.target.value as FileType)}
+          >
+            <option value="CSV">CSV</option>
+            <option value="JSON">JSON</option>
+            <option value="TSV">TSV</option>
+          </select>
+          <br />
+          <br />
+          <label htmlFor="filename">Filename</label>
+          <br />
+          <input
+            id="filename"
+            type="text"
+            value={filename}
+            onChange={(e) => setFilename(e.target.value)}
+          />
+          <br />
+          <br />
+          <FileDownloadLink
+            setsDataAsyncInOnClick
+            columnNames={parsedData.columnNames}
+            rows={parsedData.rows}
+            filename={filename}
+            fileType={fileType}
+            onClick={() => {
+              setParsedData({
+                columnNames: JSON.parse(columnNames),
+                rows: JSON.parse(rows),
+              });
+            }}
+          >
+            <button>Download dynamic data</button>
+          </FileDownloadLink>
+        </div>
+        <div>
+          <h2>Download static data</h2>
+          <FileDownloadLink
+            fileType="CSV"
+            columnNames={["Fruit", "Price"]}
+            filename="fruits"
+            rows={[
+              ["Apple", 1],
+              ["Watermelon", 5],
+              ["Durian", 10],
+            ]}
+          >
+            Download static data
+          </FileDownloadLink>
+        </div>
+      </div>
+    </>
   );
 }
