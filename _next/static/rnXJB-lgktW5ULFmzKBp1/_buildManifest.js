fe8130bd5ba728f494a31c19570d92d3445e76ea
erik_ritter <erik.ritter@airbnb.com>
2022-12-27 16:53:48 -0800

    Deploy

---
+++ _next/static/rnXJB-lgktW5ULFmzKBp1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-33ad24fb00f99551.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
